548e41ab674aa28710af5104ee7ce330a87c6c92
Barry Ku <barryku@tcl.com>
2015-11-15 21:14:06 -0800

    updated upload-data

--- dynamodb/upload-data.js
+++ dynamodb/upload-data.js
@@ -1,6 +1,6 @@
 var aws = require('aws-sdk');
 
-aws.config.loadFromPath('config.json');
+aws.config.loadFromPath('../config.json');
 
 var db = new aws.DynamoDB();
 
@@ -34,6 +34,36 @@ var params = {
             'PageCount': {'N': '600'},
             'InPublication': {'N': '1'},
             'ProductCategory': {'S': 'Book'}
+          }
+        }
+      },
+      {
+        PutRequest: {
+          Item: {
+            'Id': {'N': '103'},
+            'Title': {'S': 'Book 103 Title'},
+            'ISBN': {'S': '333-111111111'},
+            'Authors': {'SS': ['Author1', 'Author2']},
+            'Price': {'N': '2000'},
+            'Dimensions': {'S': '8.5 x 11.0 x 1.5'},
+            'PageCount': {'N': '600'},
+            'InPublication': {'N': '0'},
+            'ProductCategory': {'S': 'Book'}
+          }
+        }
+      },
+      {
+        PutRequest: {
+          Item: {
+            'Id': {'N': '201'},
+            'Title': {'S': '18-Bike-201'},
+            'Description': {'S': '201 Description'},
+            'BicycleType': {'S': 'Road'},
+            'Brand': {'S': 'Mountain A'},
+            'Price': {'N': '100'},
+            'Gender': {'S': 'M'},
+            'Color': {'SS': ['Red', 'Black']},
+            'ProductCategory': {'S': 'Bicycle'}
           }
         }
       }
